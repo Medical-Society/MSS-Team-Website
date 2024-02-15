d0eeaee1b37bd6c85cf514a509ff16fff031f7d2
Efraim Nabil <efraimefefraim@gmail.com>
2024-02-15 17:35:12 +0200

    Fix authentication check in Navbar component

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -20,14 +20,14 @@ const Navbar = ({}: IProps) => {
     Cookies.remove('token');
     Cookies.remove('admin');
   }
-
+  console.log(auth);
   return (
     <div className="fixed top-0 z-20 left-0 w-full drop-shadow-sm p-4 flex justify-between items-center backdropfilter backdrop-blur-sm"> 
       <h1 className="text-black">Navbar</h1>
         <div className="flex gap-4">
             <NavLink to="/" className="text-xl text-black hover:text-primary active:text-primary">Home</NavLink>
             {
-              auth ? <NavLink to="/login" className="text-xl text-black hover:text-primary active:text-primary">Login</NavLink> 
+              !auth.token ? <NavLink to="/login" className="text-xl text-black hover:text-primary active:text-primary">Login</NavLink> 
               : 
               <button 
                 className="text-xl text-black hover:text-primary active:text-primary"
